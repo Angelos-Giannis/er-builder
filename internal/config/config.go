4acfb8f944287307dd0afef6c4125cceeb85aeca
Angelos Giannis <angelos.giannis@gmail.com>
2020-08-29 23:55:02 +0200

    Upgrade to version v0.6.1

--- internal/config/config.go
+++ internal/config/config.go
@@ -35,7 +35,7 @@ func New() Config {
 			},
 			Name:    "ErBuilder",
 			Usage:   "CLI tool to automatically generate a .er file including a list of model files",
-			Version: "v0.6.0",
+			Version: "v0.6.1",
 		},
 		Settings: settings{
 			AllowedColumnNameCaseValues: []string{"snake_case", "camelCase", "screaming_snake_case", "kebab_case"},
